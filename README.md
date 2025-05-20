e97865274d0569b038f00074bbd253d2a1fd4a82
HyeonSeong <gus8131@naver.com>
2025-05-20 22:04:02 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Metabask-be
---
+++ src/test/java/project/metabaskbe/MetabaskBeApplicationTests.java
@@ -0,0 +1,13 @@
+package project.metabaskbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetabaskBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
