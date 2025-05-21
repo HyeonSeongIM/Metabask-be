4fd4e6e71f33cd646091c2947ab69c202412a313
HyeonSeong <gus8131@naver.com>
2025-05-21 19:14:39 +0900

    [#1] Feat : Init

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.4.5"
     id("io.spring.dependency-management") version "1.1.7"
+    kotlin("jvm")
 }
 
 group = "project"
@@ -30,6 +31,10 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("org.web3j:core:5.0.0")
+    implementation(kotlin("stdlib-jdk8"))
+
 }
 
 tasks.withType<Test> {
